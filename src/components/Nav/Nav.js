d22f77edbbeffe24f464ce425414b7c12a6650e7
Marble <mrivera0014@gmail.com>
2022-01-23 18:33:46 -0700

    updated resume link

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import * as FaIcons from 'react-icons/fa'
 import './Nav.css'
+// import { IconBase } from 'react-icons/lib'
 
 
 function Nav() {
@@ -9,16 +10,17 @@ function Nav() {
         <nav className="topnav">
             <Link to="/" replace>
                 Home
-                        </Link>
+            </Link>
             <Link to="/projects" replace>
                 Projects
-                        </Link>
+            </Link>
             <Link to="/contact" replace>
                 Contact
-                        </Link>
+            </Link>
 
             <a rel="noreferrer" target="_blank" href="https://github.com/mrivera0014"> <FaIcons.FaGithub /> </a>
             <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/marblerivera/"> <FaIcons.FaLinkedinIn /> </a>
+            <a rel="noreferrer" target="_blank" href="https://docs.google.com/document/d/1wGAY2TkO1PHy_BMIvL2pVQVSt3viq4cJlt27ien5QnQ/edit?usp=sharing"> <FaIcons.FaAlignJustify /> Resume </a>
 
         </nav>
     )
