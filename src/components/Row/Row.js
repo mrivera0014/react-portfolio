2a9b755971592d629f9f06dbe3fd030ebcaa78ea
Marble <mrivera0014@gmail.com>
2022-01-23 18:06:27 -0700

    removing old bio

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -9,7 +9,7 @@ function Rows(props) {
         <Row>
             {props.children}
             <section className="aboutMe meSection">
-                <p >Hello, my name is Maribel Rivera although I do prefer to go by Marble! I'm from Denver, CO. and worked
+                {/* <p >Hello, my name is Maribel Rivera although I do prefer to go by Marble! I'm from Denver, CO. and worked
                     at Starbucks for about five years. I also have experience with Microsoft Word, Excel, and AutoCAD. I
                     first became interested about coding about two years ago when a friend talked to me about going to
                     school to become a web developer. I decided to look into it more because it seemed very interesting. The
@@ -17,7 +17,7 @@ function Rows(props) {
                     ideas for projects that I could do if I did decide to take on this challenge. After a few months of
                     planning out how to go about learning coding I decided that this was the right move to make, of course
                     the pandemic happened and delayed my plans a little bit. Today I am going through the University of
-                    Denver's coding bootcamp and I am excited to see the progress I make throughout the course.</p>
+                    Denver's coding bootcamp and I am excited to see the progress I make throughout the course.</p> */}
 
                 <p>
                     Hello, my name is Maribel Rivera although I do prefer to go by Marble!
